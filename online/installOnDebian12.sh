91112252e44aa1c1d178aeb04545b0e4a514e294
Bolo101 <iglesias.guillaumedu64@gmail.com>
2024-10-31 21:38:07 +0100

    Updated

--- online/installOnDebian12.sh
+++ online/installOnDebian12.sh
@@ -19,7 +19,7 @@ if [ ! -f "$MARKER_FILE" ]; then
 
     apt install wget -y
 
-    sleep(5)
+    sleep 5
 
     # Ajouter la clé du dépôt Proxmox
     wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
@@ -27,11 +27,11 @@ if [ ! -f "$MARKER_FILE" ]; then
     # Mettre à jour le dépôt et le système
     apt update && apt full-upgrade -y
 
-    sleep(5)
+    sleep 5
 
     # Installer le noyau Proxmox
     apt install proxmox-default-kernel -y
-    sleep(5)
+    sleep 5
     # Installer les paquets GRUB
     apt install grub2-common -y
 
