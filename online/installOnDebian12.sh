bd497073fad7cd3ba0b2a4041c8cb1f7581654f9
Bolo101 <iglesias.guillaumedu64@gmail.com>
2024-11-01 10:33:27 +0100

    Updated network to test

--- online/installOnDebian12.sh
+++ online/installOnDebian12.sh
@@ -66,24 +66,23 @@ else
 
     # Terminer les configurations de base
     apt autoremove -y
-    cat <<EOL > /etc/network/interfaces
-
-auto lo
-iface lo inet loopback
-
-auto $SECOND_INTERFACE
-iface $SECOND_INTERFACE inet static
-
-auto vmbr0
-iface vmbr0 inet static
-    address 192.168.122.146/24
-    gateway 192.168.122.1
-    bridge-ports $SECOND_INTERFACE
-    bridge-stp off
-    bridge-fd 0
-
-source /etc/network/interfaces.d/*
-EOL
+ {
+        echo "auto lo"
+        echo "iface lo inet loopback"
+        echo
+        echo "auto $SECOND_INTERFACE"
+        echo "iface $SECOND_INTERFACE inet dhcp"
+        echo
+        echo "auto vmbr0"
+        echo "iface vmbr0 inet static"
+        echo "    address 192.168.122.146/24"
+        echo "    gateway 192.168.122.1"
+        echo "    bridge-ports $SECOND_INTERFACE"
+        echo "    bridge-stp off"
+        echo "    bridge-fd 0"
+        echo
+        echo "source /etc/network/interfaces.d/*"
+    } > /etc/network/interfaces || { echo "Failed to write /etc/network/interfaces"; exit 1; }
 
     sed -i 's/127.0.1.1/192.168.122.146/g' /etc/hosts    
     
