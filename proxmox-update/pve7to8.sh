a92979ce59447d7252809d73257ec10b5c2a2d81
Bolo101 <iglesias.guillaumedu64@gmail.com>
2025-01-04 22:47:51 +0100

    Updated to root execution on proxmox VE server

--- proxmox-update/pve7to8.sh
+++ proxmox-update/pve7to8.sh
@@ -1,8 +1,8 @@
 echo "Updating source..."
-sudo apt update
+apt update
 sleep 1
 echo "Updating current packages..."
-sudo apt dist-upgrade
+apt dist-upgrade
 echo "Update debain sources from bullseye to bookworm..."
 sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list
 echo "Done!"
@@ -10,10 +10,10 @@ echo "Adding Ceph package repository"
 echo "deb http://download.proxmox.com/debian/ceph-quincy bookworm no-subscription" > /etc/apt/sources.list.d/ceph.list
 echo "Done"
 echo "Updating new source repositories..."
-sudo apt update
+apt update
 echo "Done"
 echo "Updating packages from new sources..."
-sudo apt dist-upgrade 
+apt dist-upgrade 
 echo "Done"
 echo "Requesting system poweroff for clean reboot"
 sleep 2
